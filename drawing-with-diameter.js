1c8f6b1182d3ae954bb0a6a7dc5fa83110d56472
timmy851 <jmelizanis@gmail.com>
2018-02-05 23:06:24 -0800

    Update drawing-with-diameter.js

--- drawing-with-diameter.js
+++ drawing-with-diameter.js
@@ -1,1 +1,22 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
 
+var diameter = 30;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == '1') {
+    fill(244, 158, 66);
+  } else if (key == '2') {
+    fill(229, 45, 125);
+  } else if (key == '3') {
+    fill(141, 37, 252); 
+  }
+}
