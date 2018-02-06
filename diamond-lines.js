6b68b012b39ad158fe47baae6f0f3294a3b33024
timmy851 <jmelizanis@gmail.com>
2018-02-05 18:39:02 -0800

    Update diamond-lines.js

--- diamond-lines.js
+++ diamond-lines.js
@@ -1,1 +1,23 @@
 
+var locx = width/2
+var loxy = 0
+var widthx = width/2
+var heighty = 0
+
+line (locx, loxy, widthx, heighty)
+
+while (heighty <= width/2){
+line (locx, loxy, widthx, heighty); 
+  locx = locx - width/20;
+  loxy = loxy + height/20;
+  widthx = widthx + width/20;
+  heighty= heighty + width/20;
+}
+
+while (heighty <= width){
+line (locx, loxy, widthx, heighty); 
+  locx = locx + width/20;
+ loxy = loxy + height/20;
+  widthx = widthx - width/20;
+  heighty= heighty + width/20;
+}
