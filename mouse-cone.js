4fb8c0e421e8ac0559e84e730b9612b91049dfdb
timmy851 <jmelizanis@gmail.com>
2018-02-05 22:48:34 -0800

    Update mouse-cone.js

--- mouse-cone.js
+++ mouse-cone.js
@@ -1,1 +1,10 @@
 
+// this one has really confused me.
+// i tried changing the cordinates to the mouse function but it does not seem to work
+var lines = 20
+
+while (lines <= height-30) {
+line (mouseX/2, 0, mouseY, height);
+lines = lines + 10
+}
+
